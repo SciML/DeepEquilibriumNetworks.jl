0836bcdb3fdabe2abd9c68dfb216b45aa55bf243
Avik Pal <avikpal@iitk.ac.in>
2021-09-16 11:58:33 -0400

    Fix gradients

--- src/FastDEQ.jl
+++ src/FastDEQ.jl
@@ -1,7 +1,6 @@
 module FastDEQ
 
 using CUDA
-using DiffEqFlux
 using DiffEqSensitivity
 using Flux
 using OrdinaryDiffEq
@@ -28,15 +27,19 @@ end
 Flux.trainable(deq::DeepEquilibriumNetwork) = (deq.p,)
 
 function (deq::DeepEquilibriumNetwork)(x::AbstractArray{T}, p = deq.p) where {T}
-    _x = zero(x)
+    z = deq.re(p)(zero(x), x)
     # Solving the equation f(u) - u = du = 0
     dudt(u, _p, t) = deq.re(_p)(u, x) .- u
-    ssprob = SteadyStateProblem(ODEProblem(dudt, _x, (zero(T), one(T)), p))
-    return solve(ssprob, deq.args...; u0 = _x, deq.kwargs...).u
+    ssprob = SteadyStateProblem(ODEProblem(dudt, z, (zero(T), one(T)), p))
+    return solve(ssprob, deq.args...; u0 = z, deq.kwargs...).u
 end
 
-# Eg:
+
+# using OrdinaryDiffEq, SteadyStateDiffEq, Flux, CUDA, Zygote, FastDEQ
+# x = rand(Float32, 1, 1)
+# model = DeepEquilibriumNetwork(Chain(Dense(1, 2), Dense(2, 1)), DynamicSS(Tsit5(); abstol = 1f-1, reltol = 1f-1)))
 # model = DeepEquilibriumNetwork(Parallel(+, Dense(1, 1), Dense(1, 1)), DynamicSS(Tsit5(); abstol = 1f-1, reltol = 1f-1))
+# Zygote.gradient(() -> model(x), Flux.params(model))
 
 export DeepEquilibriumNetwork
 
