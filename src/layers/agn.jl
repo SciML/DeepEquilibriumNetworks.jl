4db5239518993221bf6bcb417e3ddfdef77c51a2
Avik Pal <avikpal@iitk.ac.in>
2021-11-02 12:19:20 -0400

    Dummy agn file

---
+++ src/layers/agn.jl
@@ -0,0 +1,3 @@
+# Code adapted from https://github.com/Chemellia/AtomicGraphNets.jl/blob/main/src/layers.jl
+# Layers have been GPUified (will try to get them upstreamed)
+# AGN stands for AtomicGraphNets
