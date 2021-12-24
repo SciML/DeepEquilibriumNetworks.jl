ea1e5de321908fb959806377f10515a19ec27e56
Avik Pal <avikpal@iitk.ac.in>
2021-12-24 14:29:39 -0500

    Invoke GC more frequently?

--- scripts/cifar_mdeq_no_array.sh
+++ scripts/cifar_mdeq_no_array.sh
@@ -4,8 +4,7 @@
 #SBATCH --gres=gpu:volta:2
 #SBATCH -n 6 -N 1
 #SBATCH --time=144:00:00
-#SBATCH -a 1-3
-#SBATCH -o slurm_logs/cifar_mdeq-%j-%a.log
+#SBATCH -o slurm_logs/cifar_mdeq-%j.log
 
 cd /home/gridsan/apal/research/FastDEQ.jl
 
