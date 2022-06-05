9aed671f458e4ff3e3a44e4bebd9db536aea2b2c
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-06-05 07:38:11 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,7 @@
 using Documenter, DocumenterCitations, FastDEQ
 
 bib = CitationBibliography(joinpath(@__DIR__, "ref.bib"), sorting = :nyt)
+include("pages.jl")
 
 makedocs(
     bib,
@@ -12,17 +13,7 @@ makedocs(
     format = Documenter.HTML(analytics = "UA-90474609-3",
                              #  assets = ["assets/favicon.ico"],
                              canonical="https://fastdeq.sciml.ai/stable/"),
-    pages = [
-        "FastDEQ: Fast Deep Equilibrium Networks" => "index.md",
-        "API" => [
-            "Dynamical Systems" => "api/solvers.md",
-            "Non Linear Solvers" => "api/nlsolve.md",
-            "General Purpose Layers" => "api/layers.md",
-            "DEQ Layers" => "api/deqs.md",
-            "Miscellaneous" => "api/misc.md",
-        ],
-        "References" => "references.md",
-    ]
+    pages = pages
 )
 
 deploydocs(
