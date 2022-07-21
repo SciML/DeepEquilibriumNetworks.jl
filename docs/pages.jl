c41a3e1c1b78312647f2c3e5fe6eabf7901ce730
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-07-21 07:13:59 -0400

    typo in pages

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,10 +1,10 @@
-pages=[
-           "Home" => "index.md",
-           "Manual" => [
-             "Dynamical Systems" => "manual/solvers.md",
-             "Non Linear Solvers" => "manual/nlsolve.md",
-             "DEQ Layers" => "manual/deqs.md",
-             "Miscellaneous" => "manual/misc.md",
-           ],
-           "References" => "references.md",
-         ])
+pages = [
+  "Home" => "index.md",
+  "Manual" => [
+    "Dynamical Systems" => "manual/solvers.md",
+    "Non Linear Solvers" => "manual/nlsolve.md",
+    "DEQ Layers" => "manual/deqs.md",
+    "Miscellaneous" => "manual/misc.md",
+  ],
+  "References" => "references.md",
+]
