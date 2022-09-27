571bf1a1418333b77474d2020b52ee8055f474b4
Avik Pal <avikpal@mit.edu>
2022-09-27 12:47:27 -0400

    Update defaults

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
 using Documenter, DocumenterCitations, DeepEquilibriumNetworks
 
-cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
-cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
+cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml"; force=true)
+cp("./docs/Project.toml", "./docs/src/assets/Project.toml"; force=true)
 
 bib = CitationBibliography(joinpath(@__DIR__, "ref.bib"); sorting=:nyt)
 
