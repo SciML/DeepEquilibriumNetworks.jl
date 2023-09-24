c99e1329b4520d56d67743538743ed799ad486f3
Arno Strouwen <arno.strouwen@telenet.be>
2023-09-24 06:35:23 +0200

    Documenter 1.0 upgrade
    a

--- docs/make.jl
+++ docs/make.jl
@@ -7,25 +7,14 @@ bib = CitationBibliography(joinpath(@__DIR__, "ref.bib"); style=:authoryear)
 
 include("pages.jl")
 
-makedocs(bib;
+makedocs(;
     sitename="Fast Deep Equilibrium Networks",
     authors="Avik Pal et al.",
-    clean=true,
-    doctest=false,
     modules=[DeepEquilibriumNetworks],
-    strict=[
-        :doctest,
-        :linkcheck,
-        :parse_error,
-        :example_block,
-        # Other available options are
-        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block,
-        # :footnote, :meta_block, :missing_docs, :setup_block
-    ],
-    checkdocs=:all,
-    format=Documenter.HTML(;
-        assets=["assets/favicon.ico"],
+    clean=true, doctest=false, linkcheck = true,
+    format=Documenter.HTML(;assets=["assets/favicon.ico"],
         canonical="https://docs.sciml.ai/DeepEquilibriumNetworks/stable/"),
+    plugins = [bib],
     pages)
 
 deploydocs(; repo="github.com/SciML/DeepEquilibriumNetworks.jl.git", push_preview=true)
