64f84a127a6109e276cf0793e56502c9b6ad994a
Avik Pal <avikpal@mit.edu>
2023-12-21 11:26:39 -0500

    Default to using SimpleGMRES for the backward pass

---
+++ ext/DeepEquilibriumNetworksLinearSolveSciMLSensitivityExt.jl
@@ -0,0 +1,18 @@
+module DeepEquilibriumNetworksLinearSolveSciMLSensitivityExt
+
+# Linear Solve is a dependency of SciMLSensitivity, so we only need to load SciMLSensitivity
+# to load this extension
+using LinearSolve, SciMLBase, SciMLSensitivity
+import DeepEquilibriumNetworks: __default_sensealg
+
+@inline function __default_sensealg(prob::SteadyStateProblem)
+    # We want to avoid the cost for cache construction for linsolve = nothing
+    # For small problems we should use concrete jacobian but we assume users want to solve
+    # large problems with this package so we default to GMRES and avoid runtime dispatches
+    linsolve = SimpleGMRES{true}(; blocksize=prod(size(prob.u0)[1:(end - 1)]))
+    linsolve_kwargs = (; maxiters=10, abstol=1e-3, reltol=1e-3)
+    return SteadyStateAdjoint(; linsolve, linsolve_kwargs, autojacvec=ZygoteVJP())
+end
+@inline __default_sensealg(::ODEProblem) = GaussAdjoint(; autojacvec=ZygoteVJP())
+
+end
